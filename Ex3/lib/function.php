3eb6840f047fccf37acdb8ee424579515e1b1b4d
leivamunoz_miguel <a24lemumi@iesgrancapitan.org>
2025-12-10 23:09:07 +0100

    Delete Ex3 directory

--- Ex3/lib/function.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    function clearData($cadena){
-        $cadena_limpia = trim($cadena);
-        $cadena_limpia = htmlspecialchars($cadena_limpia);
-        $cadena_limpia = stripslashes($cadena_limpia);
-        return $cadena_limpia;
-    }
